fee61fceae63d5eb8ce3d567e159b837063df3e3
Vlad Romila <romilavlad2001@gmail.com>
2019-02-23 18:26:51 +0200

    Added new commands

--- SLID_CLI/index.js
+++ SLID_CLI/index.js
@@ -3,51 +3,81 @@ const program = require("commander");
 const express = require("express");
 const bodyParser = require("body-parser");
 const opn = require("opn");
+const firebase = require("firebase");
+const fs = require("fs");
+var readlineSync = require('readline-sync');
 
+firebase.initializeApp({
+    apiKey: "AIzaSyBZwaUfj4RaI9kVGXWgHUz23jroUGd-mn0",
+    authDomain: "slidalbums.firebaseapp.com",
+    databaseURL: "https://slidalbums.firebaseio.com",
+    projectId: "slidalbums",
+    storageBucket: "slidalbums.appspot.com",
+    messagingSenderId: "167009021016"
+})
 let app = express();
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get("/",(req,res,next)=>{
-    res.send("<h1>This is the slid server!</h1>")
+
+app.get("/", (req, res, next) => {
+    res.render("index.html");
 })
-program
-    .command('start')
-    .description('Server start')
-    .action(() => {
-        app.listen(5100, () => {
-            console.log("The slid server has already starterd");
-            opn("http://localhost:5100");
+const login = () => {
+    var email = readlineSync.question('Album Email: ');
+    var password = readlineSync.question('Album password: ');
+    firebase.auth().signInWithEmailAndPassword(email.trim(), password.trim())
+        .then((data) => {
+            fs.writeFile(__dirname + "/userdata.json", JSON.stringify({ email: email, password: password }), function (err) {
+
+                if (err) {
+                    return console.log(err);
+                }
+
+                if (readlineSync.keyInYN('User has been saved. Do you want to start the server?') === true) {
+                    app.listen(5100, () => {
+                        console.log("The slid server has already started");
+                        opn("http://localhost:5100");
+                    })
+                }
+            })
         })
-    })
+        .catch(() => {
+            console.log("There was an error signing in your accout.")
+        })
+}
 program
-    .command('next <uid>')
-    .description('Slide next')
-    .action((uid) => {
-        request.post({
-            headers: { 'content-type': 'application/json' },
-            url: 'https://slidserver.herokuapp.com/controlSlide',
-            body: JSON.stringify({
-                albumUid: uid,
-                type: "nextSlide"
-            })
-        }, function (error, response, body) {
-            process.exit();
-        });
-    })
+    .command("login")
+    .action(() => login())
 program
-    .command('prev <uid>')
-    .description('Slide next')
-    .action((uid) => {
-        request.post({
-            headers: { 'content-type': 'application/json' },
-            url: 'https://slidserver.herokuapp.com/controlSlide',
-            body: JSON.stringify({
-                albumUid: uid,
-                type: "previousSlide"
-            })
-        }, function (error, response, body) {
-            process.exit();
-        });
+    .command("start")
+    .action(() => {
+        fs.readFile(__dirname + '/userdata.json', function read(err, data) {
+            if (err) {
+                console.log("You need to sign in in order to start the server!")
+                if (readlineSync.keyInYN('Do you want to sign in?') === true)
+                    login();
+                else
+                    process.exit();
+            }
+            else {
+                firebase.auth().signInWithEmailAndPassword(JSON.parse(data).email, JSON.parse(data).password)
+                    .then(() => {
+                        app.listen(5100, () => {
+                            console.log("The slid server has already starterd");
+                            opn("http://localhost:5100");
+                        })
+                    })
+                    .catch(() => {
+                        console.log("The credentials you have previously entered are now invalid.")
+                        if (readlineSync.keyInYN('Do you want to sign in again?') === true)
+                            login();
+                        else
+                            process.exit();
+                    })
+            }
+        })
+
+
     })
-program.parse(process.argv);
+program.parse(process.argv)
