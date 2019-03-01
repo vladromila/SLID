5b4dff6233ed6c6c0e5518a05cd077740b608270
alexmedve <medve_a@yahoo.com>
2019-03-01 16:29:55 +0200

    working modal (not styled)

--- SLID_FRAMEWORK_GRID/main.js
+++ SLID_FRAMEWORK_GRID/main.js
@@ -4,6 +4,7 @@ const show = document.querySelector(".show");
 const mask = document.querySelector(".mask");
 const close = document.querySelector(".close");
 const imgs = document.querySelectorAll(".imgs .imagecont");
+const modal = document.querySelector(".modal .modalcont img");
 
 // functions
 
@@ -12,12 +13,18 @@ closeModal = () => {
 };
 
 imgClick = (event) => {
-    const current = document.querySelector("#current");
-    const source = event.target.src;
-    current.src = source;
+    source = event.target.getAttribute('src');
+    if(source === null)
+    {
+        c = event.target.childNodes;
+        source = c[1].getAttribute('src'); 
+    }
     mask.classList.add("active");
     console.log(source);
+    modal.setAttribute("src", source);
 };
+
+//event listeners
 
 imgs.forEach(img => img.addEventListener("click", imgClick));
 
